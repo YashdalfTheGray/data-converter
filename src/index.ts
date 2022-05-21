afd4e6bef4b15b86de6d42244f6774074afb2a37
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-05-21 07:37:56 -0700

    added the right wiring for preact

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
+import { render } from 'preact';
+
 import App from './components/App';
 
-const app = new App({});
-
-document.querySelector('div#app-root')!.innerHTML = app.render();
+render(App, document.querySelector('div#app-root')!);
