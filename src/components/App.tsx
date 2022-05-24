cd27d8b0edaceedad29d564ea5d2c67b1fd8ed30
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-05-24 07:45:20 -0700

    added the header component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import { Component } from 'preact';
 
+import Header from './Header';
+
 import './app.scss';
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1 className="header">Data Converter</h1>
+        <Header />
         <p className="main">This is where the main content will go</p>
         <p className="footer">This is a footer</p>
       </div>
