511c1533bd068ac76bcf63e3ae8b6a591c998afc
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-05-25 08:27:47 -0700

    added a component for footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'preact';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <p>This is where the footer is going to to go</p>
+      </div>
+    );
+  }
+}
