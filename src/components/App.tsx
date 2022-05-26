3d19a5a7c9ed0b014fe8b4ae70739892b94ac987
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-05-26 08:58:18 -0700

    imported codepanes into the project

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import { Component } from 'preact';
 
 import Header from './Header';
 import Footer from './Footer';
+import CodePanes from './CodePanes';
 
 import './app.scss';
 
@@ -10,7 +11,7 @@ export default class App extends Component {
     return (
       <div className="app">
         <Header />
-        <div className="main">This is where the main content will go</div>
+        <CodePanes />
         <Footer />
       </div>
     );
