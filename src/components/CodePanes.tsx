76ac0d5558af805e932cf391b87e09f0d2c7d43e
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-05-26 08:58:06 -0700

    added a component for the code panes

---
+++ src/components/CodePanes.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'preact';
+
+export default class CodePanes extends Component {
+  render() {
+    return (
+      <div className="main">
+        <div>The code panes will go here</div>
+      </div>
+    );
+  }
+}
