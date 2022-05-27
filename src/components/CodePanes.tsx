9fa037486a5996149afc3a312926a62f59510a8c
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-05-27 03:39:06 -0700

    pull in the dummy data, format as json, render, for now

--- src/components/CodePanes.tsx
+++ src/components/CodePanes.tsx
@@ -1,10 +1,23 @@
 import { Component } from 'preact';
 
-export default class CodePanes extends Component {
+import { data as sampleData } from 'data-converter/sampleData';
+import { JsonValue } from 'data-converter/types';
+
+export type CodePanesState = {
+  currentData: JsonValue;
+};
+export default class CodePanes extends Component<{}, CodePanesState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { currentData: sampleData };
+  }
+
   render() {
+    const { currentData } = this.state;
+    console.log(currentData);
     return (
       <div className="main">
-        <div>The code panes will go here</div>
+        <pre>{JSON.stringify(currentData, null, 2)}</pre>
       </div>
     );
   }
