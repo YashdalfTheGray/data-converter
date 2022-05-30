a54f2877a7d8f6889bfd2a59e9d91b143b06eded
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-05-29 23:31:51 -0700

    added some styles to the footer bit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,12 @@
 import { Component } from 'preact';
 
+import './Footer.scss';
+
 export default class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        <div>This is where the footer is going to to go</div>
+        This is where the footer is going to to go
       </div>
     );
   }
