491ce25da964094eb458158d5586a9f005f88c47
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-05-31 06:30:15 -0700

    added a bit of detail about the project in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,13 @@ export default class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        This is where the footer is going to to go
+        This application converts code using&nbsp;
+        <a href="">
+          <code>js-yaml</code>
+        </a>
+        &nbsp;and does not communicate with a server. You can find the
+        code&nbsp;
+        <a href="">here</a>.
       </div>
     );
   }
