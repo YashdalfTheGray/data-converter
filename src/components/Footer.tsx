09f77edfa7205c467422b087a750788574ec8e4d
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-06-01 02:23:29 -0700

    clarified the footer message a little more, made it smaller

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,12 +6,11 @@ export default class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        This application converts code using&nbsp;
+        This application does conversions using&nbsp;
         <a href="https://github.com/nodeca/js-yaml">
           <code>js-yaml</code>
         </a>
-        &nbsp;and does not communicate with a server. You can find the
-        code&nbsp;
+        &nbsp;in the browser itself. You can find the code&nbsp;
         <a href="https://github.com/YashdalfTheGray/data-converter">here</a>.
       </div>
     );
